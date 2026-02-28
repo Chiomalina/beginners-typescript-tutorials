4ca42bd959c1cb246a6cb0571d1eef1ace57f9d7
iamchioma <chiomalinaanaso@gmail.com>
2026-02-28 17:41:31 +0100

    feat(typeScript): adding arrays

--- codefinity/arrays.ts
+++ codefinity/arrays.ts
@@ -2,3 +2,9 @@ let number: number[] = [1, 2, 3];
 let newLength = number.push(4);
 console.log(newLength);
 console.log(number);
+
+// Removing the last element
+let numbers: number[] = [1, 2, 3, 4];
+let lastNumber = numbers.pop()
+console.log(lastNumber);
+console.log(numbers);
