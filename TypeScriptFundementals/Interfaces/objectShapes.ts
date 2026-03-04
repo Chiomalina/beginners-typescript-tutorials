91f945f9be8f25e4d5b399ff60569c5a71a8366c
iamchioma <chiomalinaanaso@gmail.com>
2026-03-04 04:14:04 +0100

    define object shapes with interfaces 2

--- TypeScriptFundementals/Interfaces/objectShapes.ts
+++ TypeScriptFundementals/Interfaces/objectShapes.ts
@@ -9,3 +9,16 @@ function printUserInfo(user:User): void {
 
 const user1 = { name: "Chioma", age: 88}
 printUserInfo(user1)
+
+
+interface User {
+  name: string;
+  age: number;
+}
+
+function greetUser(user: User): string {
+  return `Hello, ${user.name}! You are ${user.age} years old.`;
+}
+
+const user2 = { name: "Bob", age: 25 };
+console.log(greetUser(user2));
