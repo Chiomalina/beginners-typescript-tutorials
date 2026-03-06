e33943884cb67377686e6c370d74e28c3424c06f
iamchioma <chiomalinaanaso@gmail.com>
2026-03-06 05:02:34 +0100

    feat(typeScript-Types): practice boolean types

---
+++ codefinity/CoreTypeScriptTypes/booleanTypes.ts
@@ -0,0 +1,16 @@
+// Declaring boolean variables
+let isActive: boolean = true;
+let hasPermission: boolean = false;
+
+// Using booleans in conditional logic
+if(isActive) {
+    console.log("The feature is active.");
+} else {
+    console.log("The feature is not active.");
+}
+
+if(hasPermission) {
+    console.log("Access granted.")
+} else {
+    console.log("Access denied.");
+}
