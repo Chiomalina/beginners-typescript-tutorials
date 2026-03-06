352dfa7ff20c10153c1cc49ba9e1456afcabeef3
iamchioma <chiomalinaanaso@gmail.com>
2026-03-06 04:54:40 +0100

    feat(typeScript-Types): practice string types

---
+++ codefinity/CoreTypeScriptTypes/stringTypes.ts
@@ -0,0 +1,10 @@
+// Declaring string variables with different syntaxes
+let singleQuoteString: string ="Hello, TypeScript!";
+let doubleString: string = "Welcome to the sring type.";
+let templateLiteralsString: string = `This is a template literal.`;
+
+// Using template literals for string interpolation
+
+let userName: string = 'Chioma';
+let greeting: string = `Hello, ${userName}!`;
+console.log(greeting); 
