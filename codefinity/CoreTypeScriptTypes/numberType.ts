d3b491d9a448e2f4b53edb84302d76ad078ec8bf
iamchioma <chiomalinaanaso@gmail.com>
2026-03-06 04:47:15 +0100

    feat(typeScript-Types): practice number types

---
+++ codefinity/CoreTypeScriptTypes/numberType.ts
@@ -0,0 +1,15 @@
+// Declaring number variables
+let age: number = 30;
+let price: number = 19.99;
+
+// Arithmetic Operations
+let total: number = age + price;
+let difference: number = age - price;
+let product: number = age * 2;
+let quotient: number = age / 3;
+
+// Output values
+console.log("Total:", total);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
